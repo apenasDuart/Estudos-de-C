33be70397a4a54cda2bd53fb3bd381df361afdde
Gabriel <131497779+apenasDuart@users.noreply.github.com>
2023-04-23 09:30:03 -0300

    This program uses while and if/else.
    I only study if/else in college, but I try to update my algorithms myself. The simple structure without the repeating loops would be:
    #include <iostream>
    using namespace std;
    int main(){
    	char a;
    	cout << "Olá... que turno você estuda? M - Matutino | V - Verspertino | N - Noturno" << endl;
    	cin >> a;
    	if (a == 'm'){
    		cout << "Bom dia!\n";
    	} else if (a == 'v'){
    		cout <<"Boa tarde!\n";
    	} else if (a == 'n'){
    		cout << "Boa noite!\n";
    	} else {
    	cout <<	"Valor inválido.\n";
    	}
    system("pause");
     return 0;
    }
    
    Important, it would be possible to do this algorithm with a string, but I would change the form of 'a' to "a" and I will still try to improve the algorithm for the case sensitive function.

---
+++ Which shift you study?.cpp
@@ -0,0 +1,26 @@
+// Libraries and funciton start
+#include <iostream>
+using namespace std;
+
+int main(){
+	// Variables 
+	char v;
+	// Input and Output
+	cout << "Type in which shift you study (M= Morning | A= Afternoon | N= Night):\n";
+	cin >> v;
+	// ERROR
+	while(v != 'M' && v != 'A' && v!= 'N'){
+		cout << "Invalid value. Please type again:\n";
+		cin >> v;
+	}
+	// Conditional
+	if(v == 'M'){
+		cout << "Good Morning!\n";
+	} else if (v == 'A') {
+	cout << "Good Afternoon!\n";
+	} else {
+		cout << "Good Night!\n";
+	}
+ system ("pause");
+return 0;
+}
